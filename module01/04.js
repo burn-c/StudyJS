cb9de8d9be3476ce7cf223ad4e0d633003249202
burn-c <chopcsom@gmail.com>
2020-04-05 19:29:48 -0300

    EX: 4

---
+++ module01/04.js
@@ -0,0 +1,26 @@
+// From 0 ~ 1        year: Beginner
+// From 1 ~ 3       years: Intermediate
+// From 3 ~ 6       years: Advanced
+// From 7 years and above: Jedi Master
+
+
+function experience(age) {
+    console.log(age);
+
+    if (age <= 1) {
+        console.log("Beginner");
+    } else if (age > 1 && age <= 3) {
+        console.log("Intermedate");
+    } else if (age > 3 && age <= 6) {
+        console.log("Advanced");
+    } else if (age >= 7) {
+        console.log("Jedi Master");
+    }   else {
+        console.log("Error");
+    }
+}
+
+var yearsOfStudy = 8;
+
+experience(yearsOfStudy);
+
