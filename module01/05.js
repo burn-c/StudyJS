a8115ab68d97050e7db2cdbc9bdeadea124dc146
burn-c <chopcsom@gmail.com>
2020-04-05 19:49:11 -0300

    EX: 5

---
+++ module01/05.js
@@ -0,0 +1,19 @@
+var users = [  
+    {    
+        name: "Diego",    
+        skills: ["Javascript", "ReactJS", "Redux"]  
+    },  
+    {    
+        name: "Gabriel",    
+        skills: ["VueJS", "Ruby on Rails", "Elixir"]  
+    }
+];
+
+function describe(users) {
+
+    for ( const user of users) {
+        console.log(`${user.name} has the skills: ${user.skills.join(', ')}.`);
+    }
+}
+
+describe(users);
