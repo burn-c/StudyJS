636e937b7f5eba1cbce4c8b989769fbec7edd324
burn-c <chopcsom@gmail.com>
2020-04-13 02:24:07 -0300

    Feacture: Typing objects and vectors

--- bootcamp/module01/typescript/src/routes.ts
+++ bootcamp/module01/typescript/src/routes.ts
@@ -1,5 +1,16 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: 'carlos@carlos.com',
+    password: '123456',
+    techs: [
+      'ReactJS',
+      { title: 'Javascript', experience: 100 },
+      { title: 'PHP', experience: 89 },
+    ],
+  });
+
   return response.json({ message: 'Hello World'});
 }
