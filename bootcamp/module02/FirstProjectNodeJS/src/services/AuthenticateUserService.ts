a244eaaf6c438d62e48da8bc26add9a7eecf4389
burn-c <chopcsom@gmail.com>
2020-04-16 21:15:10 -0300

    Feacture: Generating JWT Token

--- bootcamp/module02/FirstProjectNodeJS/src/services/AuthenticateUserService.ts
+++ bootcamp/module02/FirstProjectNodeJS/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,9 +30,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    // User authenticated
+    const token = sign({}, '2136b15089507d5b617a7388a7bdf1bf', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
