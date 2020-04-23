dee0346e6911cd9e32b5380a0006b259e4f60997
burn-c <chopcsom@gmail.com>
2020-04-22 23:21:24 -0300

    Feacture: Repository Router

--- bootcamp/module03/05-first-react-project/src/pages/Repository/index.tsx
+++ bootcamp/module03/05-first-react-project/src/pages/Repository/index.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
 
-const Repository: React.FC = () => <h1>Repository</h1>;
+interface RepositoryParams {
+  respository: string;
+}
+
+const Repository: React.FC = () => {
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return <h1>Repository: {params.respository}</h1>;
+};
 
 export default Repository;
