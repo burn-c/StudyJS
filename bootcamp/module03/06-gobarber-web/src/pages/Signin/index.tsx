9a53711831f3199439c90f8b76987246544d2f5c
burn-c <chopcsom@gmail.com>
2020-04-26 23:33:49 -0300

    Feature: Components Button and Input

--- bootcamp/module03/06-gobarber-web/src/pages/Signin/index.tsx
+++ bootcamp/module03/06-gobarber-web/src/pages/Signin/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 
 import { Container, Content, Background } from './styles';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 const SignIn: React.FC = () => (
   <Container>
@@ -12,11 +14,16 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu logon</h1>
 
-        <input placeholder="E-mail" />
+        <Input icon={FiMail} name="email" placeholder="E-mail" />
 
-        <input type="password" placeholder="Senha" />
+        <Input
+          icon={FiLock}
+          name="password"
+          type="password"
+          placeholder="Senha"
+        />
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
