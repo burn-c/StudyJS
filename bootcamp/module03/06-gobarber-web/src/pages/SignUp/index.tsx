cf11447a6bba6445a639e1bba03230c26a56ded7
burn-c <chopcsom@gmail.com>
2020-04-26 23:44:16 -0300

    Feature: SignUp page

---
+++ bootcamp/module03/06-gobarber-web/src/pages/SignUp/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Content, Background } from './styles';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Background />
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+
+      <form>
+        <h1>Faça seu cadastro</h1>
+
+        <Input icon={FiUser} name="name" placeholder="Nome" />
+        <Input icon={FiMail} name="email" placeholder="E-mail" />
+
+        <Input
+          icon={FiLock}
+          name="password"
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button type="submit">Cadastrar</Button>
+      </form>
+
+      <a href="login">
+        <FiArrowLeft />
+        Voltar para logon
+      </a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;
