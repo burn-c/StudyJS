eefc16e57da92b5fd5e80e7ae0c8d1fc34ac5054
burn-c <chopcsom@gmail.com>
2020-05-05 21:51:25 -0300

    Feature: Dependency Inversion Priciple

--- bootcamp/module02/FirstProjectNodeJS/src/modules/appointments/services/CreateAppointmentService.ts
+++ bootcamp/module02/FirstProjectNodeJS/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,10 +1,9 @@
 import { startOfHour } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 
 import AppError from '@shared/errors/AppError';
 
-import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import Appointment from '../infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '../repositories/IAppointmentService';
 
 /**
  * Receiving information
@@ -12,18 +11,18 @@ import Appointment from '../infra/typeorm/entities/Appointment';
  * Access to the repository
  */
 
-interface Request {
+interface IRequest {
   provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ date, provider_id }: Request): Promise<Appointment> {
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  constructor(private appointmentsRepository: IAppointmentsRepository) {}
 
+  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     );
 
@@ -32,7 +31,7 @@ class CreateAppointmentService {
     }
 
     // Create and save
-    const appointment = await appointmentsRepository.create({
+    const appointment = await this.appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     });
