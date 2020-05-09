595ebb2d66e02793ae5f68bed1e598472a27e43b
burn-c <chopcsom@gmail.com>
2020-05-08 23:51:37 -0300

    Feacture: Dependency injection

--- bootcamp/module02/FirstProjectNodeJS/src/modules/users/infra/http/routes/users.routes.ts
+++ bootcamp/module02/FirstProjectNodeJS/src/modules/users/infra/http/routes/users.routes.ts
@@ -5,6 +5,7 @@ import uploadConfig from '@config/upload';
 import CreateUserService from '@modules/users/services/CreateUserService';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
+import { container } from 'tsyringe';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import UsersRepository from '../../typeorm/repositories/UsersRepository';
 
@@ -15,8 +16,7 @@ const upload = multer(uploadConfig);
 usersRouter.post('/', async (request, response) => {
   const { name, email, password } = request.body;
 
-  const usersRepository = new UsersRepository();
-  const createUser = new CreateUserService(usersRepository);
+  const createUser = container.resolve(CreateUserService);
 
   const user = await createUser.execute({
     name,
@@ -34,9 +34,7 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    const usersRepository = new UsersRepository();
-
-    const updateUserAvatar = new UpdateUserAvatarService(usersRepository);
+    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
 
     const user = await updateUserAvatar.execute({
       user_id: request.user.id,
--- bootcamp/module02/FirstProjectNodeJS/src/modules/users/services/AuthenticateUserService.ts
+++ bootcamp/module02/FirstProjectNodeJS/src/modules/users/services/AuthenticateUserService.ts
@@ -4,6 +4,7 @@ import authConfig from '@config/auth';
 
 import AppError from '@shared/errors/AppError';
 
+import { injectable, inject } from 'tsyringe';
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
 
@@ -17,8 +18,12 @@ interface IResponse {
   token: string;
 }
 
+@injectable()
 class AuthenticateUserService {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
 
   public async execute({ email, password }: IRequest): Promise<IResponse> {
     const user = await this.usersRepository.findByEmail(email);
