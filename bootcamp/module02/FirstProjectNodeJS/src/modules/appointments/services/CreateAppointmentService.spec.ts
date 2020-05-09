3c35c89fdc335ed46d59611075b509812bdd776a
burn-c <chopcsom@gmail.com>
2020-05-09 00:57:43 -0300

    TEST: First test

--- bootcamp/module02/FirstProjectNodeJS/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ bootcamp/module02/FirstProjectNodeJS/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,6 +1,20 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', () => {
-    expect().toBe();
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '1231321321313',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('1231321321313');
   });
 
   // it('should NOT be able to create two appointments on the same time', () => {
