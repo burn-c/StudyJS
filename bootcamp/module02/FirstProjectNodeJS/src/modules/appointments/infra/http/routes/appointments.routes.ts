c11e238e4c4307b4e421130df6a03707dc3f40fd
burn-c <chopcsom@gmail.com>
2020-05-09 00:10:28 -0300

    Feacture: Use controllers

--- bootcamp/module02/FirstProjectNodeJS/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ bootcamp/module02/FirstProjectNodeJS/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,37 +1,14 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
-
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
-import { container } from 'tsyringe';
+import AppointmentController from '../../controllers/AppointmentsController';
 
 const appointmentsRouter = Router();
 
 // Middleware
 appointmentsRouter.use(ensureAuthenticated);
-
-// // List appointments
-// appointmentsRouter.get('/', async (request, response) => {
-//   const appointments = await appointmentsRepository.find();
-
-//   return response.json(appointments);
-// });
+const appointmentsController = new AppointmentController();
 
 // Create appointment
-appointmentsRouter.post('/', async (request, response) => {
-  const { provider_id, date } = request.body;
-
-  const parsedDate = parseISO(date);
-
-  const createAppointment = container.resolve(CreateAppointmentService);
-
-  const appointment = await createAppointment.execute({
-    date: parsedDate,
-    provider_id,
-  });
-
-  return response.json(appointment);
-});
+appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
