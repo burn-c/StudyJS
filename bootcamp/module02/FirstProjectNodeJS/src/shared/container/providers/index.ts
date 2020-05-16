76faaa6e29445d4617d4280c827253b98e0cd124
burn-c <chopcsom@gmail.com>
2020-05-15 21:57:37 -0300

    Feature: E-mail template

--- bootcamp/module02/FirstProjectNodeJS/src/shared/container/providers/index.ts
+++ bootcamp/module02/FirstProjectNodeJS/src/shared/container/providers/index.ts
@@ -1,16 +1,23 @@
 import { container } from 'tsyringe';
 
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsmailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsmailTemplateProvider';
+import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
 
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsmailTemplateProvider,
+);
+
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  new EtherealMailProvider(),
+  container.resolve(EtherealMailProvider),
 );
