843b43ddc591455ba23f495a8e9e0c7f58817b02
burn-c <chopcsom@gmail.com>
2020-05-19 00:02:43 -0300

    Feature: Controllers and Routes

--- bootcamp/module02/FirstProjectNodeJS/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ bootcamp/module02/FirstProjectNodeJS/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,13 +1,25 @@
 import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../../controllers/ProvidersController';
+import ProviderMonthAvailabilityCaontroller from '../../controllers/ProviderMonthAvailabilityCaontroller';
+import ProviderDayAvailabilityCaontroller from '../../controllers/ProviderDayAvailabilityCaontroller';
 
 const providersRouter = Router();
 
 // Middleware
 providersRouter.use(ensureAuthenticated);
 const providersController = new ProvidersController();
+const providerMonthAvailabilityCaontroller = new ProviderMonthAvailabilityCaontroller();
+const providerDayAvailabilityCaontroller = new ProviderDayAvailabilityCaontroller();
 
 providersRouter.get('/', providersController.index);
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityCaontroller.index,
+);
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityCaontroller.index,
+);
 
 export default providersRouter;
