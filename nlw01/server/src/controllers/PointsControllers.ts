0f3118c292fe3354af71fea9fd842ee8daa130a9
burn-c <chopcsom@gmail.com>
2020-06-05 23:52:11 -0300

    Feature: Image upload

--- nlw01/server/src/controllers/PointsControllers.ts
+++ nlw01/server/src/controllers/PointsControllers.ts
@@ -19,9 +19,15 @@ class PointsController {
     .distinct()
     .select('points.*');
 
-    console.log(city, uf, items);
 
-    return response.json(points);
+    const serializedPoints = points.map(point => {
+      return {
+        ...points,
+        image_url: `${process.env.URL}uploads/${point.image}`,
+      };
+    });
+
+    return response.json(serializedPoints);
   }
 
   async show(request: Request, response: Response) {
@@ -33,12 +39,19 @@ class PointsController {
      return response.status(400).json({ message: 'Point not found.' });
    }
 
+
+   const serializedPoint = {
+      ...point,
+      image_url: `${process.env.URL}uploads/${point.image}`,
+  };
+
+
    const items = await knex('items')
    .join('point_items', 'items.id', '=', 'point_items.item_id')
    .where('point_items.point_id', id)
    .select('items.title');
 
-   return response.json({ point, items });
+   return response.json({ point: serializedPoint, items });
   }
 
   async create(request: Request, response: Response) {
@@ -53,12 +66,12 @@ class PointsController {
       uf,
       items
     } = request.body;
-
+    console.log(request.body)
     const trx = await knex.transaction();
 
     const point ={
       name,
-      image: 'https://images.unsplash.com/photo-1543083477-4f785aeafaa9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+      image: request.file.filename,
       email,
       whatsapp,
       latitude,
@@ -71,12 +84,15 @@ class PointsController {
 
     const point_id = insertedIds[0];
 
-    const pointItems = items.map((item_id: number)  => {
-      return {
-        item_id,
-        point_id,
-      };
-    })
+    const pointItems = items
+      .split(',')
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number)  => {
+        return {
+          item_id,
+          point_id,
+        };
+      })
 
     await trx('point_items').insert(pointItems);
 
--- nlw01/server/src/routes.ts
+++ nlw01/server/src/routes.ts
@@ -1,15 +1,21 @@
 import Router from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import PointsController from './controllers/PointsControllers';
 import ItemsController from './controllers/ItemsControllers';
 
 const routes = Router();
+const upload = multer(multerConfig);
+
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points',  pointsController.create);
 routes.get('/points',  pointsController.index);
 routes.get('/points/:id',  pointsController.show);
+
+routes.post('/points', upload.single('image'), pointsController.create);
 
 export default routes;
