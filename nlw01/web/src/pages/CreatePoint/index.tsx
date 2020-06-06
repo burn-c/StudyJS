75041aed5e872d25f4add4824a74caff027b3b95
burn-c <chopcsom@gmail.com>
2020-06-06 00:31:31 -0300

    Feature: Image upload

--- nlw01/web/src/pages/CreatePoint/index.tsx
+++ nlw01/web/src/pages/CreatePoint/index.tsx
@@ -9,6 +9,7 @@ import { Map, TileLayer, Marker } from 'react-leaflet';
 import api from '../../services/api';
 import localeapi from '../../services/localeapi';
 import { LeafletMouseEvent } from 'leaflet';
+import Dropzone from '../../components/Dropzone';
 
 interface IItem {
   id: number;
@@ -30,6 +31,7 @@ const CreatePoint = () => {
   const [cities, setCities] = useState<string[]>([]);
 
   const [selectedInitialPosition, setSelectedInitialPosition] = useState<[number, number]>([0,0]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const [formData, setFormData] = useState({
     name: '',
@@ -136,17 +138,24 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
+    const data = new FormData();
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items
-    };
+
+
+      data.append('name', name);
+      data.append('email', email);
+      data.append('whatsapp', whatsapp);
+      data.append('uf', uf);
+      data.append('city', city);
+      data.append('latitude', String(latitude));
+      data.append('longitude', String(longitude));
+      data.append('items', items.join(','));
+
+      if (selectedFile) {
+        data.append('image', selectedFile)
+      }
+
+
       await api.post('points', data);
 
       alert('Ponto de coleta criado!');
@@ -170,6 +179,8 @@ const CreatePoint = () => {
           <br/>
           ponto de coleta
         </h1>
+
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
