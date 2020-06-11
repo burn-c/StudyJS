ddb4e32695e33c55b94d507a58eba2ec85bfbaca
burn-c <chopcsom@gmail.com>
2020-06-10 23:07:48 -0300

    Feature: Header

--- bootcamp/module03/appgobarber/src/pages/Dashboard/styles.ts
+++ bootcamp/module03/appgobarber/src/pages/Dashboard/styles.ts
@@ -1,39 +1,36 @@
 import styled from 'styled-components/native';
-import { getBottomSpace } from 'react-native-iphone-x-helper';
-import { Platform } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
-  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
 `;
 
-export const Title = styled.Text`
-  font-size: 24px;
-  color: #f4ede8;
-  font-family: 'RobotoSlab-Medium';
-  margin: 64px 0 24px;
-`;
+export const Header = styled.View`
+  padding: 24px;
+  padding-top: ${getStatusBarHeight() + 24}px;
+  background: #28262e;
 
-export const BackToSignIn = styled.TouchableOpacity`
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  background: #312e38;
-  border-top-width: 1px;
-  border-color: #232129;
-  padding: 16px 0 ${16 + getBottomSpace()}px;
-
-  justify-content: center;
-  align-items: center;
   flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
-export const BackToSignInText = styled.Text`
-  color: #fff;
-  font-size: 18px;
+export const HeaderTitle = styled.Text`
+  color: #f4ede8;
+  font-size: 20px;
   font-family: 'RobotoSlab-Regular';
-  margin-left: 16px;
+  line-height: 28px;
+`;
+
+export const UserName = styled.Text`
+  color: #ff9000;
+  font-family: 'RobotoSlab-Medium';
+`;
+
+export const ProfileButton = styled.TouchableOpacity``;
+
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
 `;
